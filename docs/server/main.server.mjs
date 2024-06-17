d13faf044cb364d25dc1278084d5c2029ac37af7
quyetcao <quyetcao200404@gamil.com>
2024-06-17 07:55:11 +0700

    deyloy 0111

--- docs/server/main.server.mjs
+++
@@ -1,9 +0,0 @@
-import './polyfills.server.mjs';
-import {
-  main_server_default
-} from "./chunk-BM5KTZUT.mjs";
-import "./chunk-OU7RYXMC.mjs";
-import "./chunk-EHLIJOMG.mjs";
-export {
-  main_server_default as default
-};
